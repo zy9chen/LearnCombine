02bbf1bd1971339a382735e908ff011141e97b6f
zy9chen <zy9chen@gmail.com>
2020-11-25 17:15:23 -0800

    Initial Commit

---
+++ LearnCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnCombine
+//
+//  Created by Zhaoyang Chen on 2020-11-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnCombine/LearnCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnCombineApp.swift
+//  LearnCombine
+//
+//  Created by Zhaoyang Chen on 2020-11-25.
+//
+
+import SwiftUI
+
+@main
+struct LearnCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
